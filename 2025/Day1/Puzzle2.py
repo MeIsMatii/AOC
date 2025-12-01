6909183816c81d08aa61eebd1859263a50a5664d
matii <eichbauer.mathilo@gmail.com>
2025-12-01 14:39:14 +0100

    added the testinput

--- 2025/Day1/Puzzle2.py
+++ 2025/Day1/Puzzle2.py
@@ -2,7 +2,7 @@ import os
 counter = 0 #counts how many zeroes there were
 dial = 50 #dial begins at 50
 
-with open('/home/mati/Documents/AOC/2025/Day1/puzzle1input.txt', 'r') as file:
+with open('/home/mati/Documents/AOC/2025/Day1/testInput.txt', 'r') as file:
     for line in file:
         line = line.strip() #it should be L/R, number 1-infinity
 
@@ -10,10 +10,17 @@ with open('/home/mati/Documents/AOC/2025/Day1/puzzle1input.txt', 'r') as file:
         rotations = int(line[1:]) #2nd and onward
 
         if direction == "L":
-            dial -= rotations
+            for i in range(rotations):
+                dial -= 1
+                dial = dial % 100
+                if dial == 0:
+                    counter += 1
         else:
-            dial += rotations
-
+            for i in range(rotations):
+                dial += 1
+                dial = dial % 100
+                if dial == 0:
+                    counter += 1
         
 
         dial = dial % 100

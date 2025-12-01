aaeb33c50fff130354b02171d51daa51d6b08eae
matii <eichbauer.mathilo@gmail.com>
2025-12-01 14:30:02 +0100

    Changed the rotation when its over 100 or below 0 to mod 100. Solved it

--- 2025/Day1/Puzzle1.py
+++ 2025/Day1/Puzzle1.py
@@ -7,22 +7,22 @@ with open('/home/mati/Documents/AOC/2025/Day1/puzzle1input.txt', 'r') as file:
         line = line.strip() #it should be L/R, number 1-infinity
 
         direction = line[0] #first thing
-        line = int(line[1:]) #2nd and onward
+        rotations = int(line[1:]) #2nd and onward
 
         if direction == "L":
-            dial -= line
+            dial -= rotations
         else:
-            dial += line
+            dial += rotations
+
+        
+
+        dial = dial % 100
+        
 
         if dial == 0:
             counter += 1
+        if dial < 0 or dial > 100:
+            print("somethings wrong")
+            break
 
-        if dial < 0:
-            while dial <0:
-                dial += 100
-        elif dial > 99:
-            while dial >0:
-                dial -= 100
-        print(counter, dial)
-
-
+print(counter)

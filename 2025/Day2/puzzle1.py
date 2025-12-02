b260417e0f0971a241862f9615d3f66fdbb30d64
Matii <103849762+MeIsMatii@users.noreply.github.com>
2025-12-02 11:05:12 +0000

    started coding

--- 2025/Day2/puzzle1.py
+++ 2025/Day2/puzzle1.py
@@ -1,4 +1,12 @@
-
-with open('puzzleInput.txt', 'r') as file:
-    for line in file:
-        line = line.strip() 
+testinput = [11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124]
+# with open('puzzleInput.txt', 'r') as file:
+#     for line in file:
+#         line = line.strip()
+#         line = line.split(",")
+line = testinput 
+for i in testinput:
+    idRange = line[i].split("-")
+    fromId = idRange[0]
+    toId = idRange[1]

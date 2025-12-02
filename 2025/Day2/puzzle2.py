2d6e166e96ed775be8073226c5c029a465dca7db
matii <eichbauer.mathilo@gmail.com>
2025-12-02 18:34:31 +0100

    Solved day2, i should learn to read btw

--- 2025/Day2/puzzle2.py
+++ 2025/Day2/puzzle2.py
@@ -1,20 +1,22 @@
+
 def checkRepeating(id):
     idString = str(id)
     # Check for all possible lengths of repeating patterns
     for length in range(1, len(idString) // 2 + 1):
-        # Repeat the substring (twice) and check if it matches the original string
-        if (idString[:(len(idString) // 2)] * (2)) == idString:
+        # Repeat the substring and check if it matches the original string
+        if (idString[:length] * (len(idString) // length)) == idString:
             return True
     return False
 
 total_sum = 0   
-with open('/home/mati/Documents/AOC/2025/Day2/testInput.txt', 'r') as file:
+with open('/home/mati/Documents/AOC/2025/Day2/puzzleInput.txt', 'r') as file:
     for line in file:
         line = line.strip()
-        line = line.split(",") #splits at each , because thats what its separated by
+        line = line.split(",")
          
     for i in range(len(line)):
         
+
         idRange = line[i].split("-") # idRange now is a list with fromId (at 0) and toId (at 1)
         fromId = int(idRange[0])
         toId = int(idRange[1])
@@ -22,7 +24,7 @@ with open('/home/mati/Documents/AOC/2025/Day2/testInput.txt', 'r') as file:
         while fromId <= toId: #banned stuff:
             if str(fromId)[0] == "0":
                 total_sum += fromId
-            elif checkRepeating(fromId):
+            if checkRepeating(fromId):
                 total_sum += fromId
             fromId += 1
     print(total_sum)

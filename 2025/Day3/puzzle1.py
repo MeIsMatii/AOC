adc87a751d34fa0dd60be25f652be7710b3614e0
Matii <103849762+MeIsMatii@users.noreply.github.com>
2025-12-03 07:41:28 +0000

    (maybe) solved the puzzle

--- 2025/Day3/puzzle1.py
+++ 2025/Day3/puzzle1.py
@@ -1,21 +1,23 @@
-number1 = 0
-number2 = 0
-number1index = 0
-with open('puzzleInput.txt', 'r') as file:
+inputFile = "/workspaces/AOC/2025/Day3/testInput.txt"
+
+totalSum = 0
+with open(inputFile, 'r') as file:
     for line in file:
+        number1 = 0
+        number2 = 0
+        number1index = 0
         line = line.strip() #it should be a really long string
         
         for i in range(len(line)):
-            currentJoltage = line[i]
-            if currentJoltage < number1 and i < len(line) -1:
+            currentJoltage = int(line[i])
+            if currentJoltage > number1 and i < len(line) -1:
                 number1 = currentJoltage
                 number1index = i
-
-
-
-with open('puzzleInput.txt', 'r') as file:
-    for line in file:
-        line = line.strip()
-        for i in range(number1index, len(line)):
-            if currentJoltage < number2:
-                number2 = currentJoltage
+        for i in range(number1index + 1,len(line)):
+            currentJoltage = int(line[i])
+            if number2 < currentJoltage:
+                number2 = currentJoltage            
+        print(number1, number2)
+        totalSum += 10*number1 + number2 
+        print(totalSum)
+print(number1,number2)

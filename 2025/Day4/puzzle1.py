4ee55ac784ba53d0ed0f3687769ab749031f35a3
matii <eichbauer.mathilo@gmail.com>
2025-12-06 20:46:21 +0100

    the testInput prints 10 instead of 13 :|

--- 2025/Day4/puzzle1.py
+++ 2025/Day4/puzzle1.py
@@ -73,5 +73,69 @@ for i in range(len(array)):
                     
                     if paperCounter < 4:
                         reachablePaper += 1
+    elif i == 0:
+        nextLine = array[i+1]
+        for j in range(len(array[i])):
+            paperCounter = 0
+            if j > 0 and j < len(array[i])-1:
+                if currentLine[j] == '@':
 
+                    #check around the '@' # i know, reaallyyy efficient :D
+                    paperCounter += checkForPaper(nextLine, j, 213123123132) #random wayy to big number
+                    paperCounter += checkForPaperCurrentLine(currentLine, j, 213123123132) #random wayy to big number
+                    
+                    if paperCounter < 4:
+                        reachablePaper += 1
+            elif j == 0:
+                if currentLine[j] == '@':
+
+                    #check around the '@' # i know, reaallyyy efficient :D
+                    paperCounter += checkForPaper(nextLine, j, 0)
+                    paperCounter += checkForPaperCurrentLine(currentLine, j, 0)
+                    
+                    if paperCounter < 4:
+                        reachablePaper += 1
+            
+            else:
+                if currentLine[j] == '@':
+
+                    #check around the '@' # i know, reaallyyy efficient :D
+                    paperCounter += checkForPaper(nextLine, j, len(array[i]))
+                    paperCounter += checkForPaperCurrentLine(currentLine, j, len(array[i]))
+                    
+                    if paperCounter < 4:
+                        reachablePaper += 1
+    
+    else:
+        prevLine = array[i-1]
+        for j in range(len(array[i])):
+            paperCounter = 0
+            if j > 0 and j < len(array[i])-1:
+                if currentLine[j] == '@':
+
+                    #check around the '@' # i know, reaallyyy efficient :D
+                    paperCounter += checkForPaper(prevLine, j, 213123123132) #random wayy to big number
+                    paperCounter += checkForPaperCurrentLine(currentLine, j, 213123123132) #random wayy to big number
+                    
+                    if paperCounter < 4:
+                        reachablePaper += 1
+            elif j == 0:
+                if currentLine[j] == '@':
+
+                    #check around the '@' # i know, reaallyyy efficient :D
+                    paperCounter += checkForPaper(prevLine, j, 0)
+                    paperCounter += checkForPaperCurrentLine(currentLine, j, 0)
+                    
+                    if paperCounter < 4:
+                        reachablePaper += 1
+            
+            else:
+                if currentLine[j] == '@':
+
+                    #check around the '@' # i know, reaallyyy efficient :D
+                    paperCounter += checkForPaper(prevLine, j, len(array[i]))
+                    paperCounter += checkForPaperCurrentLine(currentLine, j, len(array[i]))
+                    
+                    if paperCounter < 4:
+                        reachablePaper += 1
 print(reachablePaper)

b5df1fc5e638e0a55b04389b35c530461fd0b439
Matii <103849762+MeIsMatii@users.noreply.github.com>
2025-12-08 11:16:35 +0000

    (maybe) solved d05/1

--- 2025/Day5/puzzle1.py
+++ 2025/Day5/puzzle1.py
@@ -1,10 +1,40 @@
-inputFile = '/home/mati/Documents/AOC/2025/Day5/testInput.txt'
+inputFile = '/workspaces/AOC/2025/Day5/puzzleInput.txt'
 
-idRanges = []
+list = []
 with open(inputFile, 'r') as file:
     for line in file:
         line = line.strip()
-        ranges = line.splig('-')
-        idRanges.append(ranges)
+        ranges = line.split('-')
+        list.append(ranges)
 
-        
+productIds = []
+idRanges = []
+check = False
+for i in range (len(list)):
+    if list[i][0] == '':
+        check = True
+        continue
+    if not check: 
+        idRanges.append(list[i])
+    else:
+        productIds.append(list[i])
+
+
+validProducts = 0
+check = False
+for idIndex in range(len(productIds)): 
+    product = int(productIds[idIndex][0])
+    for i in range(len(idRanges)):
+        for j in range(int(idRanges[i][0]), int(idRanges[i][1])+1):
+            if check:
+                check = False
+                break
+            if product == j:
+                validProducts += 1
+                print(f"Valid product found: {product}")
+                check = True
+                break
+            print(f"Checking product {product} against range {j}")
+
+
+print(validProducts)

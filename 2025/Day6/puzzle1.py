1547f9cfe2580b89269e2801c3a6252c900b33c2
Matii <103849762+MeIsMatii@users.noreply.github.com>
2025-12-10 08:56:39 +0000

    solved d06/1

---
+++ 2025/Day6/puzzle1.py
@@ -0,0 +1,39 @@
+inputFile = '2025/Day6/puzzleInput.txt'
+temp = ''
+grid = []
+with open(inputFile, 'r') as file:
+    for line in file:
+        line = line.strip().split(' ')
+
+        
+        newList = []
+        for i in range(len(line)):
+            if line[i] != ' ' and line[i] != '':
+                newList.append(line[i])
+
+        grid.append(newList)
+
+print(grid)
+
+length = len(grid[0])
+totalSum = 0
+
+
+for i in range(length):
+    numbers = []
+    currentSum = 0
+    for j in range(len(grid)):
+        if j != len(grid) -1:
+            numbers.append(grid[j][i])
+        else:
+            operator = grid[j][i]
+    for j in range(len(numbers)):
+        if operator == '+':
+            currentSum += int(numbers[j])
+        elif operator == '*':
+            if currentSum == 0:
+                currentSum = 1
+            currentSum *= int(numbers[j])
+    totalSum += currentSum
+
+print(totalSum)

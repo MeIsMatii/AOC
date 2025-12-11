b14ddafbce5f3251e7ff1e79eba9565fe4939d24
Matii <103849762+MeIsMatii@users.noreply.github.com>
2025-12-11 10:09:36 +0000

    Solved d07/1

--- 2025/Day7/puzzle1.py
+++ 2025/Day7/puzzle1.py
@@ -1,4 +1,4 @@
-inputFile = '2025/Day7/testInput.txt'
+inputFile = '2025/Day7/puzzleInput.txt'
 grid = []
 with open(inputFile, 'r') as file:
     for line in file:
@@ -7,51 +7,33 @@ with open(inputFile, 'r') as file:
 
 for i in range(len(grid[0])):
     if grid[0][i] == 'S':
-        shipPosX = (i)
-    
-grid[1][shipPosX] = '|'
+        grid[1][i] = '|'
+        break
 
 splits = 0
-beams = 1
 
-for i in range(len(grid)):
-    print(''.join(grid[i]))
+# for i in range(len(grid)):
+#     print(''.join(grid[i]))
 
 for i in range(1,len(grid) -1 ):
     for j in range(len(grid[i])):
+
         if str(grid[i][j]) != '|':
             continue
 
-        for k in range(len(grid)):
-            if k == len(grid) - 1:
-                break
-            if str(grid[i+1][j]) != '^':
-                grid[i+1][j] = '|'
-            else:
-                splits += 1
-                if k - 1 >= 0:
-                    beams += 1
-                    grid[i+1][j-1] = '|'
-                if k + 1 < len(grid[i]):
-                    beams += 1
-                    grid[i+1][j+1] = '|'
-                for x in range(len(grid)):
-                    print(''.join(grid[x]))
-                print('------------')
+        if i == len(grid) - 1:
+            break
+            
+        if str(grid[i+1][j]) != '^':
+            grid[i+1][j] = '|'
+        
+        else:
+            splits += 1
+            if i - 1 >= 0:
+                grid[i+1][j-1] = '|'
+            if i + 1 < len(grid[i]):
+                grid[i+1][j+1] = '|'
 
 print(splits)   
-for i in range(len(grid)):
-    print(''.join(grid[i]))
-# beams = [shipPosX]
 # for i in range(len(grid)):
-#     if str(grid[i][shipPosX]) != '^':
-#         grid[i][shipPosX] = '|'
-#         for j in range(len(beams)):
-#             if beams[j][1] == grid[i - 1][shipPosX]:
-#                 beams[j][1] = (grid[i],shipPosX)
-#                 break
-#     else:
-#         beams.remove((i,shipPosX))
-#         beams.append((i,shipPosX - 1))
-#         beams.append((i,shipPosX + 1)) #split beam    
-
+#     print(''.join(grid[i]))
